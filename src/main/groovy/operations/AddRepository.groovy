9278b87ded40944fa1602cc8bd5c15f6b0041dc2
anna <anna@developmentontheedge.com>
2026-02-20 16:01:21 +0700

    Use user and token from systemsettings. Fix inserts.

--- src/main/groovy/operations/AddRepository.groovy
+++ src/main/groovy/operations/AddRepository.groovy
@@ -1,10 +1,19 @@
 package operations
 
+import java.sql.Timestamp
+import java.time.Instant
+import java.time.OffsetDateTime
 import java.util.Map.Entry
+
+import javax.json.JsonObject
+import javax.json.JsonString
 
 import com.developmentontheedge.be5.databasemodel.util.DpsUtils
 import com.developmentontheedge.be5.server.model.Base64File
 import com.developmentontheedge.be5.server.operations.support.GOperationSupport
+
+import com.developmentontheedge.be5.operation.OperationResult
+
 import ru.genespace.dockstore.SourceFile
 import ru.genespace.dockstore.Workflow
 import ru.genespace.dockstore.WorkflowVersion
@@ -21,24 +30,36 @@ class AddRepository extends GOperationSupport {
     Object getParameters(Map<String, Object> presetValues) throws Exception {
         presets = presetValues
         params.repositoryPath = [value: presetValues.get("repositoryPath" ),
-            DISPLAY_NAME: "Repository URL", TYPE: String]
+            DISPLAY_NAME: "Repository name (must be in format github://owner/repo)", TYPE: String]
         params.doi = [value: presetValues.get("doi" ),
             DISPLAY_NAME: "DOI", TYPE: String, CAN_BE_NULL: true]
+
         return params
     }
 
     @Override
     void invoke(Object parameters) throws Exception {
+
         def repositoryPath = params.$repositoryPath
+        def repositoryId = repositoryPath
+        if(repositoryPath.startsWith("github://")) {
+            repositoryId = repositoryPath.substring(9)
+        }
         def doi = params.$doi
         def repoID = database.repositories << [
             url  : repositoryPath,
             doi : doi
         ]
-        GitHubManagee gitHubManager = new GitHubManager("ryabova.anna@gmail.com", "token_here", null)
-        Map<String, Workflow> workflows = gitHubManager.processRepository(repositoryPath )
+        
+	//TODO: possibility to change token/user by interface
+        //def user = database.users.getBy([ user_name: userInfo.userName ])
+        def githubUser = db.getString( "SELECT setting_value FROM systemsettings WHERE section_name='registry' AND setting_name='github_user'" )
+        def githubToken = db.getString( "SELECT setting_value FROM systemsettings WHERE section_name='registry' AND setting_name='github_token'" )
+
+
+        GitHubManager gitHubManager = new GitHubManager(githubUser, githubToken)
+        Map<String, Workflow> workflows = gitHubManager.processRepository(repositoryId )
         for ( Entry<String, Workflow> e : workflows.entrySet() ) {
-            //TODO: insert into database
             /*Resources: 
              repository: repositories.ID
              name:
@@ -76,21 +97,33 @@ class AddRepository extends GOperationSupport {
                  doi: DOI for this version of Git repository 
                  */
                 //TODO: doi is not supported now
-                def versionID = database.versions.getBy( [repository: repoID, name: version.getName(), commit: version.getCommitID(),
+                def versionDB = database.versions.getBy( [repository: repoID, name: version.getName(), commit: version.getCommitID(),
                     type: version.getReferenceType().toString()])
-                if(versionID == null)
-                    versionID = database.versions << [repository: repoID, name: version.getName(), commit: version.getCommitID(),
-                        dateModified: version.getLastModified(), type: version.getReferenceType().toString()]
+                def versionID = versionDB ? versionDB.$ID : null
+                if(versionID == null) {
+                    def versionName = version.getName()!= null ? version.getName() : "name is unset";
+                    versionID = database.versions << [repository: repoID, name: versionName, commit: version.getCommitID(),
+                        dateModified: getDateTime(version.getLastModified()), type: version.getReferenceType().toString()]
+                }
                 /* ResourceToVersion
                  resource: resources.ID
                  version: versions.ID
                  language: language version used by corresponding resource
-                 valid: a version is valid if the descriptor file(s) have been successfully validated         
-                 doi: DOI for this resource of this version of Git repository     
+                 valid: ENUM(yes, no)  a version is valid if the descriptor file(s) have been successfully validated         
+                 doi: DOI for this resource of this version of Git repository
+                 snapshot: ENUM(yes, no) indicates that this version of the resource is snapshot     
                  */
-                //TODO: doi and language
-                database.resource2version << [resource: wflID, version:versionID, valid: version.isValid() ]
+                //TODO: doi, language, snapshot
+                def valid = version.isValid() ? 'yes' : 'no'
+                database.resource2versions << [resource: wflID, version:versionID, valid: version.isValid() ? 'yes' : 'no' ]
             }
         }
+        setResult(OperationResult.finished())
+    }
+
+    private Timestamp getDateTime(Date date) {
+
+        Instant instant = date.toInstant();
+        return Timestamp.from( instant );
     }
 }
